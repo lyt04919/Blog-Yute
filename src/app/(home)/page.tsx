fe16e6117caa4d8e29ac06d9bc5ef22259a9454c
Yuteng Li <2648517638@qq.com>
2026-09-02 14:33:39 +0800

    layout(home): move Digital Favorites Bento Grid section to the bottom of homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -62,7 +62,22 @@ export default function Home() {
 				{/* 1. 原版第一个页面：3D 卡片与双层 Orbit 卫星环绕系统 */}
 				<HeroSection />
 
-				{/* 2. 灵感与收藏全景看板：Magic UI Bento Grid (Favorites: Books, Movies, Music, Games) */}
+				{/* 2. 项目展示：Selected Works 3 联 App Browser Mockup */}
+				<LazySection minHeight="620px">
+					<EditorialProjects />
+				</LazySection>
+
+				{/* 3. 足迹航线：Nomad Trajectory 真实地理矢量地图与图钉挂照 */}
+				<LazySection minHeight="720px">
+					<NomadTrajectoryMap />
+				</LazySection>
+
+				{/* 4. 影音书香客厅：Audio & Cinema Lounge 黑胶唱机与全分类画廊展架 */}
+				<LazySection minHeight="850px">
+					<AudioCinemaLounge />
+				</LazySection>
+
+				{/* 5. 灵感与收藏全景看板：Magic UI Bento Grid (Favorites: Books, Movies, Music, Games) */}
 				<LazySection minHeight="760px">
 					<section className="w-full max-w-4xl mx-auto px-4 sm:px-6 mt-16 sm:mt-20 pt-6">
 						<div className="flex flex-col items-center text-center mb-8 sm:mb-12">
@@ -78,21 +93,6 @@ export default function Home() {
 						</div>
 						<GeekIdentityBento />
 					</section>
-				</LazySection>
-
-				{/* 3. 项目展示：Selected Works 3 联 App Browser Mockup */}
-				<LazySection minHeight="620px">
-					<EditorialProjects />
-				</LazySection>
-
-				{/* 4. 足迹航线：Nomad Trajectory 真实地理矢量地图与图钉挂照 */}
-				<LazySection minHeight="720px">
-					<NomadTrajectoryMap />
-				</LazySection>
-
-				{/* 5. 影音书香客厅：Audio & Cinema Lounge 黑胶唱机与全分类画廊展架 */}
-				<LazySection minHeight="850px">
-					<AudioCinemaLounge />
 				</LazySection>
 			</div>
 		</main>
